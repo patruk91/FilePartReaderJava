fdf48cf9b677df8a57e43e3545ec02921c8d5b07
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-29 08:43:03 +0200

    Setup inital paramters: file path, start and end line

--- src/main/java/FilePartReader.java
+++ src/main/java/FilePartReader.java
@@ -1,2 +1,23 @@
 public class FilePartReader {
+    private String filePath;
+    private int fromLine;
+    private int toLine;
+
+    public FilePartReader() {
+        this.filePath = "";
+        this.fromLine = 0;
+        this.toLine = 0;
+    }
+
+    private void setup(String filePath, int fromLine, int toLine) {
+        if (toLine < fromLine) {
+            throw new IllegalArgumentException("Invalid argument: fromLine is bigger than toLine");
+        } else if (fromLine < 1) {
+            throw new IllegalArgumentException("Invalid argument: fromLine is bigger than toLine");
+        } else {
+            this.filePath = filePath;
+            this.fromLine = fromLine;
+            this.toLine = toLine;
+        }
+    }
 }
