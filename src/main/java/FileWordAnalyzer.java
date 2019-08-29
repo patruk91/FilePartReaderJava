830857b3d82e76e953bb5752ded643e6be0c58ad
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-29 09:49:50 +0200

    Get words ordered alphabetically

--- src/main/java/FileWordAnalyzer.java
+++ src/main/java/FileWordAnalyzer.java
@@ -1,2 +1,20 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWordAnalyzer {
+    private FilePartReader filePartReader;
+
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+    }
+
+    public List<String> getWordsOrderedAlphabetically() throws IOException {
+        String content = filePartReader.readLines();
+        List<String> words = Arrays.asList(content.split(" "));
+        Collections.sort(words);
+        return words;
+    }
 }
